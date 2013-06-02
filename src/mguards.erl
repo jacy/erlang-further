769775b4cdfd00b64e34f27f497b2d7f9035fa21
jacy <johnsoncr.hong@gmail.com>
2013-06-03 00:07:20 +0800

    add guards.

--- src/mguards.erl
+++ src/mguards.erl
@@ -7,9 +7,13 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([]).
 
-
+%% In addition to using comparisons and Boolean evaluation in your guards, you can use math operations
+%%  (for example, A*B/C >= 0) and functions about data types, such as is_integer/1, is_atom/1, and so on. 
+%% One negative point about guards is that they will not accept user-defined functions because of side effects.
+%% Erlang is not a purely functional program- ming language (like Haskell) because it relies on side effects a lot.
+%% You can do I/O, send messages between actors, or raise exceptions as you want and when you want. 
+%% So instead, Erlang just doesn’t trust you (and it may be right not to!).
 
 %% ====================================================================
 %% Internal functions
